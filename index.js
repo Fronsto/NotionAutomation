895296a32a250447f1e44db1f600d65b69390e28
Anindya Vijayvargeeya <an.vijay.q@gmail.com>
2021-12-05 12:31:40 +0530

    Connected with clist's API📩

--- index.js
+++ index.js
@@ -15,25 +15,57 @@ const notion = new Client({
 });
 
 const block_id=process.env.NOTION_BLOCK_ID;
+const db_id=process.env.NOTION_DB_ID;
 
+let now=new Date();
+now=`${now.getUTCFullYear()}-${now.getUTCMonth()+1}-${now.getUTCDate()}T${now.getUTCHours()}%3A00`
+let tomorrow=new Date();
+tomorrow.setDate(tomorrow.getDate()+1);
+tomorrow=`${tomorrow.getUTCFullYear()}-${tomorrow.getUTCMonth()+1}-${tomorrow.getUTCDate()}T${tomorrow.getUTCHours()}%3A00`
 
-(async () => {
-  const blockId = block_id;
-  const response = await notion.blocks.update({
-    block_id: blockId,
-    paragraph: {
-      text: [{
-        type: "text",
-        text: {
-          content: "Lacinato kale"
-        }
-      }],
-      
+getFutureContests = async () => {
+    try{
+        let url= `https://clist.by/api/v2/contest/?username=${process.env.CLIST_USER}&api_key=${process.env.CLIST_API_KEY}`
+        url+= `&start__gt=${now}&end__lt=${tomorrow}`;
+        const res = await fetch(url).then(res => res.json());
+        
+        console.log(res.objects[0]);
+
+        return res.objects[1].host;
     }
-  });
-  console.log(response);
-})();
+    catch(err){
+        return err.message;
+    }
+}
+updateNotion =async (liste) => {
+    try{
+        const blockId = block_id;
+        const response = await notion.blocks.update({
+            block_id: blockId,
+            paragraph: {
+                text: [{
+                    type: "text",
+                    text: {
+                        content: liste
+                    }   
+                }],
+            }
+        });
+        return "success"
+    }
+    catch(err){
+        return err.message;
+    }
+}
+
+main = async () => {
+    const liste = await getFutureContests();
+    const result = await updateNotion(liste);
+    console.log(result);
+};
+
 
 app.listen(3000 || process.env.PORT, () => {
     console.log("Server is up and running");
+    main();
 });
