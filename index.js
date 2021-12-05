0370fc03e2238934dac1a4a03311067e9db22f63
Anindya Vijayvargeeya <an.vijay.q@gmail.com>
2021-12-05 18:51:47 +0530

    Able to add contests retrieved from clist into notion database😋

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv').config();
 const {Client} = require('@notionhq/client');
+const { response } = require('express');
 
 const express = require('express');
 const app = express();
@@ -25,33 +26,71 @@ tomorrow=`${tomorrow.getUTCFullYear()}-${tomorrow.getUTCMonth()+1}-${tomorrow.ge
 
 getFutureContests = async () => {
     try{
-        let url= `https://clist.by/api/v2/contest/?username=${process.env.CLIST_USER}&api_key=${process.env.CLIST_API_KEY}`
-        url+= `&start__gt=${now}&end__lt=${tomorrow}`;
+        const url= `https://clist.by/api/v2/contest/?username=${process.env.CLIST_USER}&api_key=${process.env.CLIST_API_KEY}&start__gt=${now}&end__lt=${tomorrow}`;
         const res = await fetch(url).then(res => res.json());
-        
-        console.log(res.objects[0]);
-
-        return res.objects[1].host;
+        return res.objects;
     }
     catch(err){
         return err.message;
     }
 }
+
+async function addItem(obj) {
+  try{
+      console.log(obj);
+    const response = await notion.pages.create({
+        parent: { database_id: db_id },
+        properties: {
+            'Name': {
+                type: 'title',
+                title: [
+                {
+                    type: 'text',
+                    text: {
+                        content: obj.event,
+                    },
+                },
+                ],
+            },
+
+            'Time': {
+                type: 'rich_text',
+                rich_text: [
+                {
+                    type: 'text',
+                    text: {
+                        content: obj.start,
+                    },
+                },
+                ],
+            },
+
+            'Link':{
+                type: 'url',
+                url: obj.href
+            },
+
+            'Host':{
+                type: 'select',
+                select: {
+                    name: obj.host,
+                },
+            },
+
+            
+        },
+    })
+    console.log("Success! Entry added.")
+  } catch (error) {
+    console.error(error.body)
+  }
+}
 updateNotion =async (liste) => {
     try{
-        const blockId = block_id;
-        const response = await notion.blocks.update({
-            block_id: blockId,
-            paragraph: {
-                text: [{
-                    type: "text",
-                    text: {
-                        content: liste
-                    }   
-                }],
-            }
-        });
-        return "success"
+        for(const obj of liste){
+            const stat = await addItem(obj);
+        }
+        return "done";
     }
     catch(err){
         return err.message;
@@ -64,6 +103,18 @@ main = async () => {
     console.log(result);
 };
 
+app.get("/api", async function(req, res){
+    try{
+
+        const respone = await notion.databases.query({
+            database_id: db_id,
+        })
+        res.send(respone);
+    }
+    catch(err){
+        return err;
+    }
+});
 
 app.listen(3000 || process.env.PORT, () => {
     console.log("Server is up and running");
