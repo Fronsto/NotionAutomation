94c8c1eeb6a52207d029642bd262dd5fd283bab6
Anindya Vijayvargeeya <an.vijay.q@gmail.com>
2021-12-06 01:18:05 +0530

    Redundant and outdated entries auto-removed, keeps the notion database fresh!

--- index.js
+++ index.js
@@ -15,13 +15,12 @@ const notion = new Client({
     auth: process.env.NOTION_TOKEN
 });
 
-const block_id=process.env.NOTION_BLOCK_ID;
 const db_id=process.env.NOTION_DB_ID;
 
 let now=new Date();
 now=`${now.getUTCFullYear()}-${now.getUTCMonth()+1}-${now.getUTCDate()}T${now.getUTCHours()}%3A00`
 let tomorrow=new Date();
-tomorrow.setDate(tomorrow.getDate()+1);
+tomorrow.setDate(tomorrow.getDate()+2);
 tomorrow=`${tomorrow.getUTCFullYear()}-${tomorrow.getUTCMonth()+1}-${tomorrow.getUTCDate()}T${tomorrow.getUTCHours()}%3A00`
 
 getFutureContests = async () => {
@@ -37,7 +36,6 @@ getFutureContests = async () => {
 
 async function addItem(obj) {
   try{
-      console.log(obj);
     const response = await notion.pages.create({
         parent: { database_id: db_id },
         properties: {
@@ -52,7 +50,6 @@ async function addItem(obj) {
                 },
                 ],
             },
-
             'Time': {
                 type: 'rich_text',
                 rich_text: [
@@ -64,20 +61,20 @@ async function addItem(obj) {
                 },
                 ],
             },
-
+            'contest_id': {
+                type: 'number',
+                number: obj.id
+            },
             'Link':{
                 type: 'url',
                 url: obj.href
             },
-
             'Host':{
                 type: 'select',
                 select: {
                     name: obj.host,
                 },
             },
-
-            
         },
     })
     console.log("Success! Entry added.")
@@ -85,11 +82,54 @@ async function addItem(obj) {
     console.error(error.body)
   }
 }
+
+precheck = async () => {
+    try{
+
+    const result = await notion.databases.query({
+        database_id: db_id,
+    });
+    let id_array =[]
+    result.results.forEach((pg)=>{
+        obj_time = new Date(pg.properties.Time.rich_text[0].text.content);
+        curr_time = new Date();
+        if(obj_time<curr_time){
+            notion.pages.update({
+                page_id: pg.id,
+                archived: true
+            })
+            console.log("removed this page")
+            console.log(pg.properties.Name.title[0].text.content)
+        } else {
+            id_array.push(pg.properties.contest_id.number);
+        }
+    })
+    return id_array ;
+    }
+    catch (err){
+        return [-1, err.message];
+    }
+
+}
+
 updateNotion =async (liste) => {
     try{
-        for(const obj of liste){
-            const stat = await addItem(obj);
+        const id_list= await precheck();
+        if(id_list[0] === -1){
+            return id_list[1];
         }
+        for(const obj of liste){
+            if(! id_list.includes(obj.id)) {
+                const stat = await addItem(obj);
+                console.log("following obj added!")
+                console.log(obj.id)
+            }
+            else{
+                console.log("already in db!")
+                console.log(obj.id)
+            }
+        }
+
         return "done";
     }
     catch(err){
@@ -105,10 +145,9 @@ main = async () => {
 
 app.get("/api", async function(req, res){
     try{
-
         const respone = await notion.databases.query({
             database_id: db_id,
-        })
+        });
         res.send(respone);
     }
     catch(err){
